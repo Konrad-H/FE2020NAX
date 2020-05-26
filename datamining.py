7092dca645e179ee66f765e943e32eb61db6b402
Konrad-H <konradhartmannv@hotmail.com>
2020-05-26 10:33:00 +0200

    whatwhat
    whatwhatwhat

--- datamining.py
+++ datamining.py
@@ -4,19 +4,15 @@ import pandas as pd
 import os
 import sys
 
-
-
 # data = pd.read_csv("example.csv") 
 # data.head()
-
 
 current_dir = os.path.dirname(__file__)
 
 oneup_dir = os.path.join(current_dir, os.path.pardir)
 
 file_path = os.path.join(oneup_dir, "./gefcom.csv")
-data = pd.read_csv(file_path) 
-print(data.head())
+df = pd.read_csv(file_path) 
+print(df.head())
 
-# 
 # with  as f:
